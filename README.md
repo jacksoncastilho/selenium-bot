3f8b3125a206741f7df6c1a83fe0058d54bcb1c3
jacksoncastilho <studyjacksoncastilho@gmail.com>
2025-08-21 12:47:11 -0400

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# selenium-bot
+# bot-reCaptcha
+
+### Web Page
+
+#### Requirements
+- PHP >= 7.4
+- Composer
+- `php-curl` extension
+
+#### Check if `php-curl` is installed
+Run the command:
+```bash
+php -m | grep curl
+````
+
+* If `"curl"` is listed, cURL is enabled.
+* If not, install it with:
+
+```bash
+sudo apt-get update && sudo apt-get install php-curl
+```
+
+#### Install Dependencies
+
+Navigate to the project directory and install dependencies with Composer:
+
+```bash
+cd app
+
+composer --version
+composer install
+```
+
+#### Environment File
+
+Copy the environment configuration file:
+
+```bash
+cp .env.example .env
+```
+
+Update `.env` with your project’s credentials and configurations.
+
+### Bot
+
+#### Requirements
+
+* Python >= 3.8
+* Virtual environment support (`venv`)
+
+#### Setup
+
+Create and activate a virtual environment:
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+Install required dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+Deactivate the virtual environment when finished:
+
+```bash
+deactivate
+```
+
+## Reference
+https://www.youtube.com/watch?v=kG_3LZ2OgBI&ab_channel=WDEV</br>
+https://www.google.com/recaptcha/admin/</br>
+https://developers.google.com/recaptcha/docs/display</br>
+https://developers.google.com/recaptcha/docs/verify</br>
+https://www.selenium.dev/documentation/</br>
+https://www.browserstack.com/guide/python-selenium-webdriver-manager</br>
+https://anti-captcha.com/pt/apidoc</br>
+https://configu.com/blog/env-in-php-the-basics-and-a-quick-tutorial/</br>
---
+++ bot.py
@@ -0,0 +1,145 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from anticaptchaofficial.recaptchav2proxyless import *
+from pathlib import Path
+from dotenv import load_dotenv
+from multiprocessing import Process
+import argparse
+import time
+import os
+import random
+
+load_dotenv()
+
+def solveCaptchaApi():
+    solver = recaptchaV2Proxyless()
+    solver.set_verbose(1)
+    solver.set_key(os.getenv('API_KEY'))
+    solver.set_website_url(args.url)
+    solver.set_website_key(dataSiteKey)
+
+    response = solver.solve_and_return_solution()
+
+    if response != 0:
+        browser.execute_script(f"document.getElementById('g-recaptcha-response').innetHTML ='{response}'")
+    else:
+        print(solver.err_string)
+
+def solveCaptcha(browser):
+    if args.version == 2:
+        clickCaptcha(browser)
+    else:
+        browser.find_element(By.XPATH, "//button[@type='submit']").click() 
+    
+    gRecaptchaResponse = ''
+
+    while gRecaptchaResponse == '':
+        gRecaptchaResponse = browser.execute_script("var elem = document.getElementById('g-recaptcha-response'); return elem ? elem.value : false;")
+        time.sleep(1)
+            
+    if(not gRecaptchaResponse):
+        print("Element 'g-recaptcha-response' not found!")
+        browser.quit()
+        exit()
+    
+    if args.version == 2:
+        browser.execute_script("document.querySelector('#submit').click()")
+
+def botAttack():
+    try:
+        if args.install:
+            browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        else:
+            browser = webdriver.Firefox()
+
+        browser.get(args.url)
+        
+        browser.find_element(By.LINK_TEXT, "About").click()
+
+        browser.find_element(By.LINK_TEXT, "Home").click()
+
+        previusElement = browser.find_element(By.ID, "controls-previus")
+        nextElement= browser.find_element(By.ID, "controls-next")
+
+        i=0
+        while i < 7:
+            if random.randint(1,2) == 1:
+                nextElement.click()
+            else:
+                previusElement.click()
+            i+=1
+
+        browser.find_element(By.LINK_TEXT, "Login").click()
+
+        time.sleep(1)
+
+        signup(browser, 1)
+
+        browser.find_element(By.LINK_TEXT, "Login").click()
+
+        time.sleep(1)
+        
+        browser.find_element(By.ID, "username").send_keys(f"{random.randint(1,999)}")
+
+        browser.find_element(By.ID, "password").send_keys("changeme")
+
+        solveCaptcha(browser)
+
+        signup(browser, 10)
+
+        browser.quit()
+    except Exception as e:
+        print(f"Error in botAttack: {e}")
+
+def clickCaptcha(browser):
+    try:
+        WebDriverWait(browser, 10).until(
+            EC.frame_to_be_available_and_switch_to_it((By.XPATH, "//iframe[@title='reCAPTCHA']"))
+        )
+
+        checkbox = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.ID, "recaptcha-anchor"))
+        )
+
+        checkbox.click()
+    finally:
+        browser.switch_to.default_content()
+
+def signup(browser, times):
+    browser.find_element(By.LINK_TEXT, "Sign Up").click()
+    
+    i=0
+    while i < times:
+        browser.execute_script(f"document.querySelector('#username').value='{random.randint(1,999)}'")
+        browser.execute_script(f"document.querySelector('#password').value='changeme'")
+        browser.execute_script(f"document.querySelector('#confirm-password').value='changeme'")
+
+        solveCaptcha(browser)
+
+        i += 1
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-url', type=str, required=True, help='Target url. E.g: http://localhost/reCaptcha-solver/index.php')
+parser.add_argument('-v', '--version', type=int, default=2, choices=[2, 3], help='Specify which version to use, available options are 2 or 3 (default: 2)')
+parser.add_argument('-i', '--install', type=bool, default=False, help='Automatically download, install and configure the appropriate browser drivers (default: False)')
+parser.add_argument('-P', '--process', type=int, default=1, help="Number of processes to spawn (default: 1).")
+parser.add_argument('-d', '--delay', type=float, default=0.1, help="Delay between spawning processes in seconds (default: 0.1).")
+
+args = parser.parse_args()
+
+processes = []
+
+for i in range(args.process):
+    process = Process(target=botAttack, daemon=True)
+    processes.append(process)
+    process.start()
+
+    time.sleep(args.delay)
+
+for process in processes:
+    if process.is_alive():
+        process.join()
